74c2f9575e075a3ec0ef2635692ca029d1062784
ThaisAlvarenga <ata381@nyu.edu>
2024-11-08 22:02:45 -0500

    Create camera.js to store all functions related to camera movement

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ let previousMouseX, previousMouseY;
 
 function preload(){
     //load skybox image --> will be used later as a texture
-    skyboxImage = loadImage('assets/sky-citiscape.png')
+    skybox1 = loadImage('assets/sky-citiscape.png')
 }
 
 function setup(){
@@ -23,7 +23,7 @@ function draw()
     // Draw the skybox
     push();
     noStroke();
-    texture(skyboxImage); //make texture out of skyboxImage
+    texture(skybox1); //make texture out of skyboxImage
     sphere(1000, 24, 16); // Using a large sphere to create the skybox effect
     pop();
 
@@ -50,59 +50,3 @@ function draw()
   // rotate camera
   cameraRotate();
 }
-
-
-function cameraMove() {
-    let speed = 3;
-    print(y);
-  
-    // WASD controls for movement
-    if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) { // W key for forward
-      x += cos(angleY) * cos(angleX) * speed;
-      z += sin(angleY) * cos(angleX) * speed;
-      // y += sin(angleX) * speed;
-    }
-    if (keyIsDown(87) || keyIsDown(UP_ARROW)) { // S key for backward
-      x -= cos(angleY) * cos(angleX) * speed;
-      z -= sin(angleY) * cos(angleX) * speed;
-      // y -= sin(angleX) * speed;
-    }
-    if (keyIsDown(65) || keyIsDown(LEFT_ARROW)) { // A key for left
-      x += cos(angleY + HALF_PI) * speed;
-      z += sin(angleY + HALF_PI) * speed;
-    }
-    if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) { // D key for right
-      x += cos(angleY - HALF_PI) * speed;
-      z += sin(angleY - HALF_PI) * speed;
-    }
-  }
-  
-  function cameraRotate() {
-    // Rotate the camera when the mouse is dragged
-    if (cameraIsRotating) {
-      let deltaX = mouseX - previousMouseX;
-      let deltaY = mouseY - previousMouseY;
-      
-      angleY -= deltaX * 0.005;
-      angleX -= deltaY * 0.005;
-  
-      // Clamp the vertical angle to prevent flipping
-      angleX = constrain(angleX, -HALF_PI, HALF_PI);
-  
-      previousMouseX = mouseX;
-      previousMouseY = mouseY;
-    }
-  }
-  
-  // Start dragging when mouse is pressed
-  function mousePressed() {
-    cameraIsRotating = true;
-    previousMouseX = mouseX;
-    previousMouseY = mouseY;
-  }
-  
-  // Stop dragging when mouse is released
-  function mouseReleased() {
-    cameraIsRotating = false;
-  }
-  
