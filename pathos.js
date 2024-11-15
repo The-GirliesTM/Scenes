f0f1b6f6ada2a4cc7ae0a5595c13699b56ae1e6a
audreycharge <64012818+audreycharge@users.noreply.github.com>
2024-11-15 18:40:34 -0500

    Update pathos.js

---
+++ pathos.js
@@ -0,0 +1,14 @@
+class Pathos {
+    constructor() {
+        this.model;
+        this.x;
+        this.y;
+        this.z;
+        this.ambientLight = 80;
+        this.inspecting = false;
+        this.left;
+        this.right;
+        this.top;
+        this.bottom;
+    }
+}
