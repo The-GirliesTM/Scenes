121c3e96a2f609df57eaf09bc063d179382ac346
audreycharge <64012818+audreycharge@users.noreply.github.com>
2024-11-18 19:12:26 -0500

    pathos interact

--- camera.js
+++ camera.js
@@ -7,6 +7,8 @@ let angleX = 0;
 let angleY = 0;
 let raycastDistance = 50;
 let isMouseLocked = false; // Flag to check if the mouse is locked. Used for camera behavior
+let lookX, lookY, lookZ;
+
 
 //Constants. Do not change during runtime.
 const speed = 3;
@@ -15,19 +17,21 @@ const rotationSpeed = 0.0025;
 
 //update location of the camera
 function cameraUpdate(cam){
-  // Define the camera position
+  // Define the camera position in world space
   let camX = x;
   let camY = y;
   let camZ = z;
   
   // Calculate where the camera is looking based on its rotation angles
-  let lookX = camX + cos(angleY) * cos(angleX);
-  let lookY = camY + sin(angleX);
-  let lookZ = camZ + sin(angleY) * cos(angleX);
+  lookX = camX + cos(angleY) * cos(angleX);
+  lookY = camY + sin(angleX);
+  lookZ = camZ + sin(angleY) * cos(angleX);
 
   // Set the camera to look from (camX, camY, camZ) towards (lookX, lookY, lookZ)
   cam.setPosition(camX, camY, camZ)
   cam.lookAt(lookX, lookY, lookZ)
+  //eyeXYZ is the camera position
+  //centerXYZ is the object position looked at
 
   //camera(camX, camY, camZ, lookX, lookY, lookZ, 0, 1, 0); //Old Camera renderer
 
@@ -79,11 +83,24 @@ function cameraRotate() {
     camX = cos(angleY) * cos(angleX)
     camY = sin(angleX)
     camZ = sin(angleY) * cos(angleX)
+    // console.log("look: "+ lookX + " : "+lookY+" : "+ lookZ)
 }
 
 function raycast() {
   //get camera anglex n angley 
   //calculate 
+  // console.log(angleX);
+  // console.log(angleY);
+  console.log(lookX + " : "+lookY+" : "+ lookZ); //this is the direction angle max 1, -50, 1
+  console.log("cam center: "+ cam.centerX + " : "+cam.centerY+" : "+ cam.centerZ)
+}
+
+function getDist(pathos) {
+  let cam_pos = createVector(cam.centerX, cam.centerY, cam.centerZ);
+  let pat_pos = createVector(pathos.x, pathos.y, pathos.z);
+  let d = cam_pos.dist(pat_pos);
+  console.log("distance: "+d);
+  return d;
 }
 
 // Automatically request pointer lock the first time mouse moves
--- pathos.js
+++ pathos.js
@@ -1,9 +1,8 @@
 class Pathos {
-    constructor(model, ox, oy, oz) {
+    constructor(model, name, ox, oy, oz) {
         this.model = model;
-        this.ox = ox;
-        this.oy = oy;
-        this.oz = oz;
+        this.name = name;
+        this.op = [ox, oy, oz];
         this.x = ox;
         this.y = oy;
         this.z = oz;
@@ -11,41 +10,74 @@ class Pathos {
         this.ambientLight = 80;
         this.inspecting = false;
         this.radius = 30;
+        this.rotation = [0, 0];
     }
     update() {
         if (this.inspecting) {
-            this.x = 0;
-            this.y = 0;
-            this.z = 0;
+            this.x = lookX;
+            this.y = lookY;
+            this.z = lookZ;
             nx +=movedX* 0.2;
             let angleX = nx/width*PI*2;
-            rotate(angleX, [0, 1, 0])
+            this.rotation[0] = angleX
 
-            if (keyIsDown(87) || keyIsDown(UP_ARROW)) { // W key for forward
+            if (keyIsDown(87) || keyIsDown(UP_ARROW)) { 
                 ny ++;
               }
-              if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) { // S key for backward
+              if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
                 ny--;
               }
             let angleY = ny/height*PI*2;
-            rotate(angleY, [1, 0, 0])
+            this.rotation[1] = angleY;
         } else {
-            this.x = this.ox;
-            this.y = this.oy;
-            this.z = this.oz;
+            this.x = this.op[0];
+            this.y = this.op[1];
+            this.z = this.op[2];
+            this.rotation = [0, 0];
+            nx = 0
+            ny = 0
         }
     }
     display() {
         push();
-        translate(this.x, this.y, this.z);
+        scale(0.2)
+        if (this.inspecting) {
+            translate(cam.eyeX, cam.eyeY, cam.eyeZ)
+            let pan = atan2(cam.eyeZ - cam.centerZ, cam.eyeX - cam.centerX)
+            let tilt = atan2(cam.eyeY - cam.centerY, dist(cam.centerX, cam.centerZ, cam.eyeX, cam.eyeZ))
+            rotateY(-pan)
+            rotateZ(tilt + PI)
+            translate(400, 0, 0)
+            rotateY(-PI/2);
+            rotateZ(PI);
+        } else {
+            translate(this.x, this.y, this.z);
+        }
+        push()
+        rotate(this.rotation[0], [0, 1, 0])
+        rotate(this.rotation[1], [1, 0, 0])
         fill(this.color);
         model(this.model);
         pop()
+        pop()
     }
-    isLookedAt() {
+    isLookedAt(ray_start, ray_dir) {
         //if vector collides then true
         //else false
         this.inspecting = true;
+        let pos = createVector(this.x, this.y, this.z)
+        s = sub(ray_start, pos);
+        b = s.dot(ray_dir);
+        c = s.dot(s) - this.radius*this.radius;
+        h = b * b - c
+        if (h < 0) { //no intersection
+            console.log("not me");
+        } else {
+            console.log("hi i'm " + this.name);
+        }
+        h = Math.sqrt(h);
+        t = -b-h;
+
     }
 
 }
